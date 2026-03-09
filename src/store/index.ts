f9b08ab1d0927af559f47a8f960d3ab750e6a63e
TheonlyGlaernisch <glaernisch.gaming@gmail.com>
2026-03-09 09:52:51 +0100

    rollback index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -10,7 +10,7 @@ export default new Vuex.Store({
   state: {
     isApplicationOn: false,
     // serverIP: 'http://'+location.hostname+':'+location.port,
-    serverIP: 'https://auto-recruitment-server.netlify.app',
+    serverIP: 'https://bar3-server.onrender.com',
     sentMessages: [],
     lastRefreshed: 0,
     packageVersion: process.env.PACKAGE_VERSION || '0',
