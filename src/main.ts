ea70dfa3234e40b2dcca1652ba77c99e39d7d044
TheonlyGlaernisch <glaernisch.gaming@gmail.com>
2026-03-11 14:06:42 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify'
-import VueGtag from "vue-gtag"
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
-Vue.use(
-  VueGtag,
-  {
-    config: { id: "G-P4D00LBHYL" }
-  },
-  router
-)
+// Google Analytics
+const script = document.createElement('script')
+script.async = true
+script.src = "https://www.googletagmanager.com/gtag/js?id=G-P4D00LBHYL"
+document.head.appendChild(script)
+
+window.dataLayer = window.dataLayer || []
+function gtag(){dataLayer.push(arguments)}
+gtag('js', new Date())
+gtag('config', 'G-P4D00LBHYL')
 
 new Vue({
   router,
