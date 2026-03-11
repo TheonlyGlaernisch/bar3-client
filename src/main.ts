84277e3287cfbe4c92f5f0297e794060e861fd42
TheonlyGlaernisch <glaernisch.gaming@gmail.com>
2026-03-11 14:21:03 +0100

    google tag added
    hope it works lol

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import VueGtag from "vue-gtag"
 
 Vue.config.productionTip = false
 
-// Google Analytics
-const script = document.createElement('script')
-script.async = true
-script.src = "https://www.googletagmanager.com/gtag/js?id=G-P4D00LBHYL"
-document.head.appendChild(script)
-
-window.dataLayer = window.dataLayer || []
-function gtag(){dataLayer.push(arguments)}
-gtag('js', new Date())
-gtag('config', 'G-P4D00LBHYL')
+Vue.use(
+  VueGtag,
+  {
+    config: { id: "G-P4D00LBHYL" }
+  },
+  router
+)
 
 new Vue({
   router,
