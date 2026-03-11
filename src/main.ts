91cbd12ed5cf6bd7710d27875bcbf1dd93329aa2
TheonlyGlaernisch <glaernisch.gaming@gmail.com>
2026-03-11 12:22:21 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,32 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import VueGtag from "vue-gtag"
 
 Vue.config.productionTip = false
 
-const GA_MEASUREMENT_ID = 'G-P4D00LBHYL'
-
-(function() {
-  const script = document.createElement('script')
-  script.async = true
-  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`
-  document.head.appendChild(script)
-
-  window.dataLayer = window.dataLayer || []
-  function gtag(){window.dataLayer.push(arguments)}
-  window.gtag = gtag
-  gtag('js', new Date())
-  gtag('config', GA_MEASUREMENT_ID)
-})()
-
-// Track route changes if using Vue Router
-router.afterEach((to) => {
-  if (window.gtag) {
-    window.gtag('config', GA_MEASUREMENT_ID, { page_path: to.fullPath })
-  }
-})
-// ------------------------
+Vue.use(
+  VueGtag,
+  {
+    config: { id: "G-P4D00LBHYL" }
+  },
+  router
+)
 
 new Vue({
   router,
