9512d89de91fc2dfeec5ca14b3bcc30a1cbe2b17
TheonlyGlaernisch <glaernisch.gaming@gmail.com>
2026-03-12 17:22:55 +0100

    Update sendConfig.ts

--- src/actions/sendConfig.ts
+++ src/actions/sendConfig.ts
@@ -1,27 +1,20 @@
 import { Config } from '../types';
-import store from '@/store';
+import { apiFetch } from '@/utilities/authFetch';
 
 export default async function sendConfig(config: Config) {
   let error;
 
-  const response = await fetch(`${store.getters.serverIP}/api/setConfig`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      config: config
-    })
-  }).catch((e) => {
+  const response = await apiFetch(
+    '/api/setConfig',
+    { method: 'POST' },
+    { config }
+  ).catch((e) => {
     error = e;
     console.error(e);
   });
 
   if (!response) return error;
-
-  if (response.status != 204) {
-    return new Error('Unexpected response code: '+response.status);
-  }
+  if (response.status !== 204) return new Error('Unexpected response code: ' + response.status);
 
   return true;
 }
