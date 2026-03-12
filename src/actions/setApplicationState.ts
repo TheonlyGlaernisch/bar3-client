603fe04bd5809bc8fa0d0bc4a7703cf76583aa3a
TheonlyGlaernisch <glaernisch.gaming@gmail.com>
2026-03-12 17:25:59 +0100

    Update setApplicationState.ts

--- src/actions/setApplicationState.ts
+++ src/actions/setApplicationState.ts
@@ -1,26 +1,19 @@
-import store from '@/store';
+import { apiFetch } from '@/utilities/authFetch';
 
 export default async function setApplicationState(applicationOn: boolean) {
   let error;
 
-  const response = await fetch(`${store.getters.serverIP}/api/setApplicationState`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      applicationOn: applicationOn
-    })
-  }).catch((e) => {
+  const response = await apiFetch(
+    '/api/setApplicationState',
+    { method: 'POST' },
+    { applicationOn }
+  ).catch((e) => {
     error = e;
     console.error(e);
   });
 
   if (!response) return error;
-
-  if (response.status != 204) {
-    return new Error('Unexpected response code: '+response.status);
-  }
+  if (response.status !== 204) return new Error('Unexpected response code: ' + response.status);
 
   return true;
 }
