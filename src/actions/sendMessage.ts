a216ca1a9f6f150f7310b97798ecc9d21b2d60b1
TheonlyGlaernisch <glaernisch.gaming@gmail.com>
2026-03-12 17:24:42 +0100

    Update sendMessage.ts

--- src/actions/sendMessage.ts
+++ src/actions/sendMessage.ts
@@ -1,29 +1,27 @@
-import store from '@/store';
+import { apiFetch } from '@/utilities/authFetch';
 
-export default async function sendMessage(messageHTML: string, nationDetails: {nationName: string; nationID: string; leaderName: string}) {
+export default async function sendMessage(
+  messageHTML: string,
+  nationDetails: { nationName: string; nationID: string; leaderName: string }
+) {
   let error;
 
-  const response = await fetch(`${store.getters.serverIP}/api/sendMessage`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      messageHTML: messageHTML,
+  const response = await apiFetch(
+    '/api/sendMessage',
+    { method: 'POST' },
+    {
+      messageHTML,
       nationID: parseInt(nationDetails.nationID),
       nationName: nationDetails.nationName,
       leaderName: nationDetails.leaderName,
-    })
-  }).catch((e) => {
+    }
+  ).catch((e) => {
     error = e;
     console.error(e);
   });
 
   if (!response) return error;
-
-  if (response.status != 204) {
-    return new Error('Unexpected response code: '+response.status);
-  }
+  if (response.status !== 204) return new Error('Unexpected response code: ' + response.status);
 
   return true;
 }
