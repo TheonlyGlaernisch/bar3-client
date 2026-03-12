41e4b6bbf510e503fb50e9aeac26c3231e7f5f51
TheonlyGlaernisch <glaernisch.gaming@gmail.com>
2026-03-12 17:22:33 +0100

    Update getConfig.ts

--- src/actions/getConfig.ts
+++ src/actions/getConfig.ts
@@ -1,26 +1,22 @@
 import { Config } from '../types';
-import store from '@/store';
+import { apiFetch } from '@/utilities/authFetch';
 
 export default async function getConfig() {
   let error;
 
-  const response = await fetch(`${store.getters.serverIP}/api/config`)
-      .catch((e) => {
-        error = e;
-        console.error(e);
-      });
+  const response = await apiFetch('/api/config').catch((e) => {
+    error = e;
+    console.error(e);
+  });
 
   if (!response) return error;
+  if (response.status !== 200) return new Error('Unexpected response code: ' + response.status);
 
-  if (response.status != 200) {
-    return new Error('Unexpected response code: '+response.status);
-  }
-
-  const config: Config | undefined = await response.json().catch((e) => {
+  const config: Config | undefined = await response.json().catch(() => {
     console.error('Could not parse config!');
+    return undefined;
   });
 
   if (!config) return false;
-
   return config;
 }
