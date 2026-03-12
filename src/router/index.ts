52d53c7ffaa7e3449abb5444150b18faa2071036
TheonlyGlaernisch <glaernisch.gaming@gmail.com>
2026-03-12 17:19:22 +0100

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,28 +1,25 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 
-import AccountManager from '../components/AccountManager.vue'
-import About from '../views/About.vue'
-import Help from '../views/Help.vue'
+import Home from '@/views/Home.vue'
+import Configuration from '@/views/Configuration.vue'
+import MessageCreator from '@/views/MessageCreator.vue'
+import Analytics from '@/views/Analytics.vue'
+import AccountManager from '@/components/AccountManager.vue'
+import About from '@/views/About.vue'
+import Help from '@/views/Help.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Account',
-    component: AccountManager
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About
-  },
-  {
-    path: '/help',
-    name: 'Help',
-    component: Help
-  },
+  { path: '/', redirect: '/dashboard' },
+  { path: '/dashboard', name: 'Dashboard', component: Home },
+  { path: '/config', name: 'Configuration', component: Configuration },
+  { path: '/message-creator', name: 'Message Creator', component: MessageCreator },
+  { path: '/analytics', name: 'Analytics', component: Analytics },
+  { path: '/account', name: 'Account', component: AccountManager },
+  { path: '/about', name: 'About', component: About },
+  { path: '/help', name: 'Help', component: Help },
 ]
 
 const router = new VueRouter({
