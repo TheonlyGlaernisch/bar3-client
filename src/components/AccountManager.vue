1ac04bf3afabd19c2253030eaf27c2cc8cd8b38a
Stefan Tufler <stefan.tufler@stud.ken.ch>
2026-03-28 10:44:13 +0100

    Commit login state to Vuex store after user load

--- src/components/AccountManager.vue
+++ src/components/AccountManager.vue
@@ -57,6 +57,7 @@ export default class AccountManager extends Vue {
       localStorage.setItem('pwAccountId', res.accountId);
       localStorage.setItem('apiKey', this.apiKey);
       this.v2Session = res.token;
+      this.$store.commit('setLoggedIn', true);
       this.statusMessage = { type: 'success', text: 'User loaded successfully' };
 
       // Immediately sync the top-right toggle state after login.
