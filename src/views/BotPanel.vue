f634a5f573f14af57a4b23a369013429e14b743b
Stefan Tufler <stefan.tufler@stud.ken.ch>
2026-05-07 16:42:17 +0200

    Fix template string syntax for server icon URL

--- src/views/BotPanel.vue
+++ src/views/BotPanel.vue
@@ -112,7 +112,7 @@ export default class BotPanel extends Vue {
   commandsError = '';
 
   serverIconUrl(server: BotServer): string {
-    return `{server.icon}.png?size=64`;
+    return `${server.icon}.png?size=64`;
   }
 
   async created() {
